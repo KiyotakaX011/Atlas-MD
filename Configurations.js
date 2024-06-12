abfe5b66fea902e0e1ede93d65c0ab616b79916b
KiyotakaX011 <142149939+KiyotakaX011@users.noreply.github.com>
2024-06-13 01:55:53 +0530

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "917819037576";   // You can replace this number with yours //
 }
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
+global.mongodb = process.env.MONGODB || "mongodb+srv://Rohit:<password>@cluster0.i2z06nv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
